59c16ba839ca776be5c0d3c3b30971860f898a18
MohammadSadawi <m.sadawi@beyegroup.com>
2026-05-21 15:51:22 +0300

    percentage

---
+++ BeyeCEO.Domain/Shared/ValueObjects/Percentage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeyeCEO.Domain.Shared.ValueObjects
+{
+    public record Percentage
+    {
+        public decimal Value { get; init; }
+
+        public Percentage(decimal value)
+        {
+            if (value < -999 || value > 999)
+                throw new ArgumentException("Percentage out of valid range");
+            Value = value;
+        }
+
+        public bool IsPositive => Value > 0;
+        public bool IsNegative => Value < 0;
+        public string Format() => $"{Value:+0.00;-0.00;0.00}%";
+        public override string ToString() => Format();
+    }
+}
